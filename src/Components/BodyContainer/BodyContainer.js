0f68ebb5a2455a708ffd5d2815193779bb1a0ead
Udit Choudhary <uditchoudhary@gmail.com>
2022-04-09 20:41:03 +1200

    fixed navigation toolbar toggler missing issue

--- src/Components/BodyContainer/BodyContainer.js
+++ src/Components/BodyContainer/BodyContainer.js
@@ -1,12 +1,17 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 80vw;
   height: 200vh;
   border: 3px solid black;
+  background: green;
 `;
 
 const BodyContainer = ({children}) => {
+    useEffect(() => {
+        window.scrollTo(0,0)
+    })
   return (
     <>
       <Container className="container-fluid">
--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useEffect, useRef } from "react";
 import styled from "styled-components";
 import logo from "../../static/images/yuuva_logo.png";
 import "./NavigationBar.css";
@@ -6,7 +7,7 @@ import "./NavigationBar.css";
 const NavbarWrapper = styled.div`
   width: 80vw;
   border-bottom: 1px solid black;
-  transition: 1s;
+  transition: 0.5s;
 `;
 
 const NavBarLogo = styled.img`
@@ -14,19 +15,38 @@ const NavBarLogo = styled.img`
   height: auto;
 `;
 
-window.onscroll = () => {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.background = "#ffffff";
-  } else {
-    document.getElementById("navbar").style.background = "none";
-  }
-};
 const NavigationBar = () => {
+  const navBarRef = useRef(null);
+
+  const onScroll = () => {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      console.log("inside if ", navBarRef);
+      navBarRef.current.style.background = "#ffffff";
+    } else {
+      console.log("inside else ", navBarRef);
+      navBarRef.current.style.background = "none";
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
     <>
-      <NavbarWrapper className="container-fluid sticky-top" id="navbar">
+      <NavbarWrapper
+        className="container-fluid sticky-top"
+        id="navbar"
+        ref={navBarRef}
+      >
         <div className="justify-content-center">
-          <nav className="navbar navbar-expand-lg d-flex">
+          <nav className="navbar navbar-expand-lg bg-inverse d-flex">
             <a className="navbar-brand me-auto ps-5" href="/">
               <NavBarLogo src={logo} alt="Logo" className="yuuva-logo" />
             </a>
@@ -39,12 +59,26 @@ const NavigationBar = () => {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span className="navbar-toggler-icon"></span>
+              {/* <span className="navbar-toggler-icon"></span> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+              </svg>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div className="ms-auto navbar-nav pe-5">
-                <Link className="nav-link" aria-current="page" to="/probiotics">
+                <Link
+                  className="nav-link ms-3"
+                  aria-current="page"
+                  to="/probiotics"
+                >
                   Probiotics
                 </Link>
                 <Link className="nav-link ms-3" to="/organics">
@@ -53,7 +87,7 @@ const NavigationBar = () => {
                 <Link className="nav-link ms-3" to="/ourroots">
                   Our Roots
                 </Link>
-                <Link className="nav-link ms-5" to="/contactus">
+                <Link className="nav-link ms-3" to="/contactus">
                   Contact Us
                 </Link>
                 <Link className="nav-link ms-3" to="/login">
