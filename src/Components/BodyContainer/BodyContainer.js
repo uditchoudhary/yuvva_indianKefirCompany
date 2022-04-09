f82f4f0f49cdeb047cf2d536963db88c7969edfa
Udit Choudhary <uditchoudhary@gmail.com>
2022-04-10 00:56:43 +1200

    added color and size of body container for demo purpose

--- src/Components/BodyContainer/BodyContainer.js
+++ src/Components/BodyContainer/BodyContainer.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 80vw;
-  height: auto;
-  background-color:red;
+  // height: auto;
+  height: 90vh;
+  background-color: #b60c2040;
 `;
 
 const BodyContainer = ({children}) => {
