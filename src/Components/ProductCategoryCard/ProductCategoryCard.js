e03eeb977657c2685c174074dda5df54a497e7b7
Udit Choudhary <uditchoudhary@gmail.com>
2022-05-30 15:17:17 +1200

    added product category card code

--- src/Components/ProductCategoryCard/ProductCategoryCard.js
+++ src/Components/ProductCategoryCard/ProductCategoryCard.js
@@ -1,24 +1,49 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
 import BodyContainer from "../../Containers/BodyContainer";
+import ProductCard from "../ProductCard";
+import { instance as API } from "../../services/axiosConfig";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 30px;
+  border: 0.1px solid black;
+`;
+
 const ProductCategoryCard = ({ productCategory }) => {
+  const dispatch = useDispatch();
+  const [isLoading, setIsloading] = useState({});
+  const [productCategoryItems, setProductCategoryItems] = useState();
+
+  useEffect(() => {
+    setIsloading(true);
+    API.get(
+      `${process.env.REACT_APP_API}/products/${productCategory.productCategory_id}`
+    ).then((res) => {
+      setProductCategoryItems(res.data);
+      setIsloading(false);
+    });
+  }, []);
+
+  console.log("--> ", productCategoryItems);
   const {
     _id,
     category_id,
     category_name,
     productCategory_id,
     productCategory_name,
-    category_description,
-    category_images,
+    productCategory_description,
+    productCategory_images,
   } = productCategory;
   return (
     <BodyContainer>
-      <h1>Product Category Card for {category_name}</h1>
-      <div key={_id}>
-        <p>{category_id}</p>
-        <p>{category_name}</p>
-        <p>{productCategory_id}</p>
-        <p>{productCategory_name}</p>
-        <p>{category_description}</p>
-      </div>
+      <Wrapper>
+        <h4 className="text-lefy">{productCategory_name}</h4>
+      </Wrapper>
+      {productCategoryItems?.map((product) => {
+        return <ProductCard product={product} />;
+      })}
     </BodyContainer>
   );
 };
